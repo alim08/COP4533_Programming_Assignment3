057d4fbc0e32cfe47235f4387c0e78171079e5e1
Adam Lim <alim4@ufl.edu>
2026-04-06 16:17:29 -0400

    Implement 2D DP table to calculate maximum subsequence value

--- src/hvlcs.py
+++ src/hvlcs.py
@@ -22,6 +22,21 @@ class HVLCS_Solver:
         self.A = lines[K + 1]
         self.B = lines[K + 2]
 
+    def solve(self):
+        m = len(self.A)
+        n = len(self.B)
+        
+        self.dp = [[0] * (n + 1) for _ in range(m + 1)]
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if self.A[i-1] == self.B[j-1]:
+                    char_val = self.values.get(self.A[i-1], 0)
+                    self.dp[i][j] = self.dp[i-1][j-1] + char_val
+                else:
+                    self.dp[i][j] = max(self.dp[i-1][j], self.dp[i][j-1])
+                    
+        return self.dp[m][n]
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
